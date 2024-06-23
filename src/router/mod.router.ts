7665e542e4d5166c57f929cecf8302960f46105c
nik-kita <nik.deno.dev@gmail.com>
2024-06-23 14:41:14 +0200

    update router, add tests

--- src/router/mod.router.ts
+++ src/router/mod.router.ts
@@ -2,7 +2,46 @@ import {createRouter, createWebHistory} from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      redirect: '/home',
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: '/home',
+    },
+    {
+      path: '/home/:username?',
+      component: {
+        template: '<h1>Home</h1>',
+      },
+      meta: {
+        public: true,
+        x_event: 'nav.Home',
+      },
+    },
+    {
+      path: '/profiles/:username?',
+      component: {
+        template: '<h1>Profiles list</h1>',
+      },
+      meta: {
+        public: true,
+        x_event: 'nav.ProfilesList',
+      },
+    },
+    {
+      path: '/profile/:name/:username?',
+      component: {
+        template: '<h1>Single profile</h1>',
+      },
+      meta: {
+        public: true,
+        x_event: 'nav.SingleProfile',
+      },
+    },
+  ],
 })
 
 export default router
--- src/xslugger.test.ts
+++ src/xslugger.test.ts
@@ -1,4 +1,4 @@
-import {expect, it, vi} from 'vitest'
+import {describe, expect, it, vi} from 'vitest'
 import {createActor, waitFor} from 'xstate'
 import {machine} from './xslugger'
 import {
@@ -8,114 +8,118 @@ import {
   type RouteMeta,
 } from 'vue-router'
 
-it.each([
-  {
-    user: {},
-    path: '/test/:username?',
-    path_to_push: '/test',
-    meta: {
-      public: true,
-      x_event: '_',
+describe('xslugger', () => {
+  it.each([
+    {
+      user: {},
+      path: '/test/:username?',
+      path_to_push: '/test',
+      meta: {
+        public: true,
+        x_event: '_',
+      },
+      expected: {
+        path: '/test',
+        role: 'user::viewer',
+      },
     },
-    expected: {
+    {
+      user: {username: 'luffy'},
+      path: '/test/:username?',
+      path_to_push: '/test/zoro',
+      meta: {
+        public: true,
+        x_event: '_',
+      },
+      expected: {
+        path: '/test/zoro',
+        role: 'user::viewer',
+      },
+    },
+    {
+      user: {username: 'luffy'},
+      path: '/test/:username?',
+      path_to_push: '/test',
+      meta: {
+        public: true,
+        x_event: '_',
+      },
+      expected: {
+        path: '/test/luffy',
+        role: 'owner',
+      },
+    },
+    {
+      path: '/test/:username?',
+      path_to_push: '/test',
+      meta: {
+        public: true,
+        x_event: '_',
+      },
+      expected: {
+        path: '/test',
+        role: 'guest::viewer',
+      },
+    },
+    {
       path: '/test',
-      role: 'user::viewer',
+      path_to_push: '/test',
+      meta: {
+        public: false,
+        x_event: '_',
+      },
+      expected: {
+        ok: false,
+      },
     },
-  },
-  {
-    user: {username: 'luffy'},
-    path: '/test/:username?',
-    path_to_push: '/test/zoro',
-    meta: {
-      public: true,
-      x_event: '_',
-    },
-    expected: {
-      path: '/test/zoro',
-      role: 'user::viewer',
-    },
-  },
-  {
-    user: {username: 'luffy'},
-    path: '/test/:username?',
-    path_to_push: '/test',
-    meta: {
-      public: true,
-      x_event: '_',
-    },
-    expected: {
-      path: '/test/luffy',
-      role: 'owner',
-    },
-  },
-  {
-    path: '/test/:username?',
-    path_to_push: '/test',
-    meta: {
-      public: true,
-      x_event: '_',
-    },
-    expected: {
+    {
+      user: {},
       path: '/test',
-      role: 'guest::viewer',
+      path_to_push: '/test',
+      meta: {
+        public: false,
+        x_event: '_',
+      },
+      expected: {
+        path: '/test',
+        role: 'owner',
+      },
     },
-  },
-  {
-    path: '/test',
-    path_to_push: '/test',
-    meta: {
-      public: false,
-      x_event: '_',
-    },
-    expected: {
-      ok: false,
-    },
-  },
-  {
-    user: {},
-    path: '/test',
-    path_to_push: '/test',
-    meta: {
-      public: false,
-      x_event: '_',
-    },
-    expected: {
-      path: '/test',
-      role: 'owner',
-    },
-  },
-] as const satisfies ({meta: RouteMeta} & Record<
-  string,
-  any
->)[])(
-  'PROCESSING: \t{ navigate_to: $path_to_push, path: $path, public: $meta.public, user: $user } \n     SHOULD PRODUCE:    { path: $expected.path, role: $expected.role }\n\n',
-  async ({meta, path, path_to_push, expected, user}) => {
-    const router = createRouter({
-      history: createMemoryHistory(),
-      routes: [
-        {
-          path,
-          meta,
-          component: {
-            template: '<div>Test</div>',
+  ] as const satisfies ({meta: RouteMeta} & Record<
+    string,
+    any
+  >)[])(
+    'PROCESSING: \t{ navigate_to: $path_to_push, path: $path, public: $meta.public, user: $user } \n     SHOULD PRODUCE:    { path: $expected.path, role: $expected.role }\n\n',
+    async ({meta, path, path_to_push, expected, user}) => {
+      const router = createRouter({
+        history: createMemoryHistory(),
+        routes: [
+          {
+            path,
+            meta,
+            component: {
+              template: '<div>Test</div>',
+            },
           },
-        },
-      ],
-    })
-    const before_each_router_mock = vi.fn(async to => {
-      const xslugger = createActor(machine, {
-        input: {
-          to,
-          user,
-        },
+        ],
       })
-      xslugger.start()
-      await waitFor(xslugger, s => s.status === 'done')
-      const snapshot = xslugger.getSnapshot()
-      expect(snapshot.output).toMatchObject(expected)
-    })
-    router.beforeEach(before_each_router_mock)
-    await router.push(path_to_push)
-    expect(before_each_router_mock).toHaveBeenCalledTimes(1)
-  },
-)
+      const before_each_router_mock = vi.fn(async to => {
+        const xslugger = createActor(machine, {
+          input: {
+            to,
+            user,
+          },
+        })
+        xslugger.start()
+        await waitFor(xslugger, s => s.status === 'done')
+        const snapshot = xslugger.getSnapshot()
+        expect(snapshot.output).toMatchObject(expected)
+      })
+      router.beforeEach(before_each_router_mock)
+      await router.push(path_to_push)
+      expect(before_each_router_mock).toHaveBeenCalledTimes(
+        1,
+      )
+    },
+  )
+})
