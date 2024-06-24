3d0076ebe45742c25d11072e1bafdceaac985a4f
nik-kita <nik.deno.dev@gmail.com>
2024-06-25 01:26:25 +0200

    feat: Add xstore service for managing user authentication and information
    This commit adds a new service called `xstore.service.ts` which provides functions for managing user authentication and information. It imports functions from `jwt-token.service` and `user-info.service` to handle refreshing tokens, updating user information, and retrieving user information. The service also includes computed properties for accessing the user's `nik` and checking if the user is authenticated. Additionally, it provides methods for cleaning and updating authentication data.
    
    Note: This commit message follows the established convention of starting with a type (feat) followed by a concise and descriptive message.

---
+++ src/services/xstore.service.ts
@@ -0,0 +1,63 @@
+import {
+  get_refresh_token,
+  update_tokens,
+} from '@/services/jwt-token.service'
+import {
+  get_user_info,
+  update_user_info,
+} from '@/services/user-info.service'
+import {computed, ref} from 'vue'
+
+const prev_session = get_refresh_token()
+const user = ref<{nik?: string | null}>()
+const nik = computed({
+  get() {
+    return user.value?.nik
+  },
+  set(nik?: string | undefined | null) {
+    if (user.value) {
+      user.value = {
+        ...user.value,
+        nik,
+      }
+    }
+  },
+})
+
+if (prev_session) {
+  user.value =
+    get_user_info() ??
+    (get_refresh_token() ? {} : undefined)
+}
+
+const is_user = computed(() => {
+  if (user.value && get_refresh_token()) {
+    return true
+  }
+
+  return false
+})
+const viewer_role = ref<
+  'guest::viewer' | 'user::viewer' | 'owner'
+>('guest::viewer')
+const nik_curr_route_param = ref(nik.value)
+
+export const use_xstore = (): x.XStore => {
+  return {
+    nik_curr_route_param,
+    is_user,
+    user,
+    nik,
+    viewer_role,
+    clean_auth() {
+      update_tokens()
+      user.value = undefined
+    },
+    update_auth(payload: api.Res<'post', '/auth/sign-in'>) {
+      update_tokens(payload)
+      const user_info = {nik: payload.nik}
+      update_user_info(user_info)
+      user.value = user_info
+    },
+  }
+}
