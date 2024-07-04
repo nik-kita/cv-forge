591a9fe5acf51237484941ca2a02e589c68f6357
nik-kita <nik.deno.dev@gmail.com>
2024-07-05 00:25:12 +0200

    add api.Res

--- src/api/api.namespace.ts
+++ src/api/api.namespace.ts
@@ -88,4 +88,57 @@ export namespace api {
       method: M
       path: P
     }
+
+  export namespace Res {
+    export type Success<
+      M extends Method,
+      P extends Path,
+      N extends
+        | 200
+        | 201
+        | 204 = 200,
+    > = paths[P][M] extends {
+      responses: Record<
+        N,
+        any
+      >
+    }
+      ? paths[P][M]['responses'][N]['content']['application/json']
+      : {
+          ERROR: `${M} ${P} => RES ${N} DOES NOT EXIST!!!`
+        }
+    export type Fail<
+      M extends Method,
+      P extends Path,
+      N extends
+        | 400
+        | 401
+        | 403
+        | 404 = 400,
+    > = paths[P][M] extends {
+      responses: Record<
+        N,
+        any
+      >
+    }
+      ? paths[P][M]['responses'][N]['content']['application/json']
+      : {
+          ERROR: `${M} ${P} => RES ${N} DOES NOT EXIST!!!`
+        }
+  }
+  export type Res<
+    M extends Method,
+    P extends Path,
+    NS extends
+      | 200
+      | 201
+      | 204 = 200,
+    NF extends
+      | 400
+      | 401
+      | 403
+      | 404 = 400,
+  > =
+    | Res.Success<M, P, NS>
+    | Res.Fail<M, P, NF>
 }
