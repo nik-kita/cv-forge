21434510f3907ef3e74cd1c708a27bbb816999ea
nik-kita <nik.deno.dev@gmail.com>
2024-07-04 16:22:43 +0200

    init

---
+++ e2e/vue.spec.ts
@@ -0,0 +1,13 @@
+import {
+  test,
+  expect,
+} from '@playwright/test'
+
+test('visits the app root url', async ({
+  page,
+}) => {
+  await page.goto('/')
+  await expect(
+    page.locator('body'),
+  ).toBeDefined()
+})
